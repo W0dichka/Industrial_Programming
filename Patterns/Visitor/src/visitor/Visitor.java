4686f26f982f9003ab31d088d74e269a68ca0d39
Kirill Kanashevich <81416978+W0dichka@users.noreply.github.com>
2023-01-12 12:48:49 +0300

    Add files via upload

---
+++ Patterns/Visitor/src/visitor/Visitor.java
@@ -0,0 +1,12 @@
+package visitor;
+import shapes.*;
+
+public interface Visitor {
+    String visitDot(Dot dot);
+
+    String visitCircle(Circle circle);
+
+    String visitRectangle(Rectangle rectangle);
+
+    String visitCompoundGraphic(CompoundShape cg);
+}
